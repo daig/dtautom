697ee0c02f00238fba8e49f696ee4914fbd13ca3
Luke H Miles <luke@cs.uky.edu>
2019-10-11 19:55:46 -0400

    renamed and rewrote instructions

--- backend.go
+++ backend.go
@@ -1,5 +1,18 @@
 // dtautom provides corewar-like functionality for decision theory dilemmas
 // Spec at alignmentforum.org/posts/qdqYrcGZTh9Lp49Nj
+
+// TODO:
+// + check within DeleteProcessor whether it exists yet
+// + think of better name that "processor". Chemical reaction as motivating analogy?
+// Ideas:
+// + Allow the agent to create and destroy money squares
+// + Make the booleans part of the number
+// + Add ability to destroy remote processors
+// + Add ability to mark cells as money but set to 0
+// + Allow the agent to read farther than it can write
+// + Make universe size and maxstep part of the universe
+// + Think of better name for money (diamonds¿)
+
 package main
 
 type number int32 // a number in dtautom
@@ -11,21 +24,25 @@ type cell struct {
 
 const maxStep number = 100 // the largest distance a processor can see, touch, or move
 const UniverseSize = 1000
+const defaultStep = 3
 const ( // all dtautom instructions
-	DIE number = iota // processor self destructs
-	DAT               // no-op or data
-	MOV               // copy from A to B
-	ADD               // add A to B
-	SUB               // subtract A from B
-	CMP               // skip next instruction if A = B
-	SLT               // skip next instruction if A < B
-	JMP               // unconditional jump to A
-	JMZ               // jump to A if B is zero
-	JMN               // jump to A if B is not zero
-	JIM               // jump to A if B is money
-	JIP               // jump to A if B is processor
-	DJN               // decrement B, then jump to A if B is not 0
-	MVM               // move money from one money slot to another money slot
+	_ number = iota
+	// unconditional operations:
+	DIE       // processor self destructs
+	DATA      // no-op or data
+	COPY      // Overwrite B with the value of A
+	TRANSFER  // add A to B and set B to 0. You can transfer money.
+	ADD       // add A to B
+	SUBTRACT  // subtract A from B
+	JUMP      // unconditional jump to A
+	MAKEPROC  // mark A as a processor cell
+	MAKEMONEY // set A to 0 and mark it as money cell
+	// conditional operations:
+	ISEQUAL // skip ahead if A = B
+	ISLESS  // skip ahead if A < B
+	ISZERO  // skip ahead if A is zero
+	ISMONEY // skip ahead if A is money
+	ISPROC  // skip ahead if A is a processor
 )
 
 func (u *Universe) AddProcessor(loc number) {
@@ -38,96 +55,89 @@ func (u *Universe) DeleteProcessor(loc number) {
 	u.memory[loc].isProcessor = false
 }
 
-func (u *Universe) Execute(loc number) {
-	// the processor always moves, so we delete the current one:
+func (u *Universe) Move(loc number, distance number) {
 	u.DeleteProcessor(loc)
+	// processor explodes if it tries to move too far
+	if abs(distance) <= maxStep {
+		u.AddProcessor((loc + distance) % UniverseSize)
+	}
+}
 
+func (u *Universe) MoveIf(loc number, b bool) {
+	if b {
+		u.Move(loc, 6)
+	} else {
+		u.Move(loc, 3)
+	}
+}
+
+func (u *Universe) Execute(loc number) {
 	instruction := u.memory[loc].n
-	Astep := u.memory[loc+1].n
-	Bstep := u.memory[loc+2].n
+	Adist := u.memory[(loc+1)%UniverseSize].n
+	Bdist := u.memory[(loc+2)%UniverseSize].n
 
-	if (Astep > maxStep || Bstep > maxStep) && instruction != DAT { // agent tries to reach too far
-		return // the agent dies
+	// Cannot read, write or jump to cells outside of range:
+	if abs(Adist) > maxStep || abs(Bdist) > maxStep {
+		u.DeleteProcessor(loc)
+		return // TODO: It should be fine if B is large if you don't use it.
 	}
 
-	Aloc := (loc + Astep) % UniverseSize
-	Bloc := (loc + Bstep) % UniverseSize
-	A := &u.memory[Aloc] // first target of operation
-	B := &u.memory[Bloc] // second target of operation
+	A := &u.memory[(loc+Adist)%UniverseSize] // first target of operation
+	B := &u.memory[(loc+Bdist)%UniverseSize] // second target of operation
 
 	switch instruction {
-	// A `return` prevents the creation of a new processor, so effectively means "die"
 	case DIE:
-		return
-	case DAT: // do nothing
-	case MOV:
+		u.DeleteProcessor(loc)
+	case DATA: // do nothing
+		u.Move(loc, 3)
+	case COPY:
 		if B.isMoney {
-			return // can't create money
+			u.DeleteProcessor(loc) // forgery is punishable by death
+		} else {
+			B.n = A.n
+			u.Move(loc, 3)
 		}
-		B.n = A.n
-	case ADD:
-		if B.isMoney {
-			return // can't create money
+	case TRANSFER:
+		if A.isMoney != B.isMoney { // both or neither must be money squares
+			u.DeleteProcessor(loc) // forgery is punishable by death
 		}
 		B.n += A.n
-	case SUB:
+		A.n = 0
+	case ADD:
 		if B.isMoney {
-			return // can't destroy money
-		}
-		B.n -= A.n
-	case CMP:
-		if A.n == B.n {
-			u.AddProcessor(loc + 6)
-			return
-		}
-	case SLT:
-		if A.n < B.n {
-			u.AddProcessor(loc + 6)
-			return
-		}
-	case JMP:
-		u.AddProcessor(Aloc)
-		return
-	case JMZ:
-		if B.n == 0 {
-			u.AddProcessor(Aloc)
-			return
-		}
-	case JMN:
-		if B.n != 0 {
-			u.AddProcessor(Aloc)
-			return
-		}
-	case JIM:
-		if B.isMoney {
-			u.AddProcessor(Aloc)
-			return
-		}
-	case JIP:
-		if B.isProcessor {
-			u.AddProcessor(Aloc)
-			return
-		}
-	case DJN:
-		B.n--
-		if B.n != 0 {
-			u.AddProcessor(Aloc)
-			return
-		}
-	case MVM:
-		// Can only move money from one money slot to another money slot
-		// Money slots cannot be created or destroyed
-		if A.isMoney && B.isMoney {
-			B.n += A.n
-			A.n = 0
+			u.DeleteProcessor(loc) // forgery is punishable by death
 		} else {
-			// If you try on non-money then you die
-			return
+			B.n += A.n
+			u.Move(loc, 3)
 		}
-	default: // invalid instruction is a DIE
-		return
+	case SUBTRACT:
+		if B.isMoney {
+			u.DeleteProcessor(loc) // forgery is punishable by death
+		} else {
+			B.n -= A.n
+			u.Move(loc, 3)
+		}
+	case JUMP:
+		u.Move(loc, Adist)
+	case MAKEMONEY:
+		A.n = 0
+		A.isMoney = true
+	case MAKEPROC:
+		A.isProcessor = true
+	case ISEQUAL:
+		u.MoveIf(loc, A.n == B.n)
+	case ISLESS:
+		u.MoveIf(loc, A.n < B.n)
+	case ISZERO:
+		u.MoveIf(loc, A.n == 0)
+	case ISMONEY:
+		u.MoveIf(loc, A.isMoney)
+	case ISPROC:
+		u.MoveIf(loc, A.isProcessor)
+	default:
+		// invalid instruction is a DIE
+		u.DeleteProcessor(loc) // TODO: this should be logged or something
 	}
-	u.AddProcessor(loc + 3)
 }
 
 type Universe struct {
@@ -139,5 +149,13 @@ type Universe struct {
 func Transition(u Universe) {
 	for p := range u.processors {
 		u.Execute(p)
+	}
+}
+
+func abs(x number) number {
+	if x > 0 {
+		return x
+	} else {
+		return -x
 	}
 }
